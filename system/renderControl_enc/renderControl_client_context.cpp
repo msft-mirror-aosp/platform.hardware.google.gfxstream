bf86237d08fae6002db4e36256a8a50f88933563
Lingfeng Yang <lfy@google.com>
2019-04-09 08:38:59 -0700

    [vulkan] Add renderControl api for setting Vulkan mode
    bug: 111137294
    bug: 130181208
    Change-Id: I0b28c9aba0b0eb2adfc85732b9ae841aece5b51a

--- system/renderControl_enc/renderControl_client_context.cpp
+++ system/renderControl_enc/renderControl_client_context.cpp
@@ -55,6 +55,7 @@ int renderControl_client_context_t::initDispatchByName(void *(*getProc)(const ch
 	rcGetColorBufferDisplay = (rcGetColorBufferDisplay_client_proc_t) getProc("rcGetColorBufferDisplay", userData);
 	rcGetDisplayPose = (rcGetDisplayPose_client_proc_t) getProc("rcGetDisplayPose", userData);
 	rcSetDisplayPose = (rcSetDisplayPose_client_proc_t) getProc("rcSetDisplayPose", userData);
+	rcSetColorBufferVulkanMode = (rcSetColorBufferVulkanMode_client_proc_t) getProc("rcSetColorBufferVulkanMode", userData);
 	return 0;
 }
 
