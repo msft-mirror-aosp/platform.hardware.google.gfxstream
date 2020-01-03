35c80f5d3af3126e0d8f0dd837c8a522466a2dde
Roman Kiryanov <rkir@google.com>
2020-01-03 21:36:31 +0000

    Revert "Switch cbmanager to HIDL HALs"
    This reverts commit bff41297bdb7c141871ea365ed34530937d90f69.
    
    Reason for revert: selinux prevents using HIDL HALs
    
    Change-Id: Iaa8a140b6aa51223b61bd66391f1cbe96ce4a4e1

--- system/include/cbmanager.h
+++ system/include/cbmanager.h
@@ -18,7 +18,6 @@
 #define ANDROID_GOLDFISH_OPENGL_SYSTEM_CBMANAGER_CBMANAGER_H
 
 #include <memory>
-#include <system/graphics.h>
 #include "gralloc_cb.h"
 
 namespace android {
@@ -34,18 +33,6 @@ public:
                                                   int height,
                                                   int format) = 0;
         virtual void freeBuffer(const cb_handle_t* h) = 0;
-
-        virtual int lockBuffer(cb_handle_t& handle,
-             const int usage,
-             const int left, const int top, const int width, const int height,
-             void** vaddr) = 0;
-
-        virtual int lockYCbCrBuffer(cb_handle_t& handle,
-             const int usage,
-             const int left, const int top, const int width, const int height,
-             android_ycbcr* ycbcr) = 0;
-
-        virtual int unlockBuffer(cb_handle_t& handle) = 0;
     };
 
     const cb_handle_t* allocateBuffer(int width, int height, int format) {
@@ -54,63 +41,6 @@ public:
 
     void freeBuffer(const cb_handle_t* h) {
         mImpl->freeBuffer(h);
-    }
-
-    int lockBuffer(cb_handle_t& handle,
-             const int usage,
-             const int left, const int top, const int width, const int height,
-             void** vaddr) {
-        return mImpl->lockBuffer(handle, usage, left, top, width, height, vaddr);
-    }
-
-    int lockBuffer(buffer_handle_t h,
-             const int usage,
-             const int left, const int top, const int width, const int height,
-             void** vaddr) {
-        cb_handle_t* cb = cb_handle_t::from_unconst(h);
-        if (cb) {
-            return lockBuffer(*cb, usage, left, top, width, height, vaddr);
-        } else {
-            return -1;
-        }
-    }
-
-
-    int lockYCbCrBuffer(cb_handle_t& handle,
-                        const int usage,
-                        const int left, const int top, const int width, const int height,
-                        android_ycbcr* ycbcr) {
-        return mImpl->lockYCbCrBuffer(handle, usage, left, top, width, height, ycbcr);
-    }
-
-    int lockYCbCrBuffer(buffer_handle_t h,
-                        const int usage,
-                        const int left, const int top, const int width, const int height,
-                        struct android_ycbcr* ycbcr) {
-        cb_handle_t* cb = cb_handle_t::from_unconst(h);
-        if (cb) {
-            return lockYCbCrBuffer(*cb, usage, left, top, width, height, ycbcr);
-        } else {
-            return -1;
-        }
-    }
-
-    int unlockBuffer(cb_handle_t& handle) {
-        return mImpl->unlockBuffer(handle);
-    }
-
-    int unlockBuffer(buffer_handle_t h) {
-        cb_handle_t* cb = cb_handle_t::from_unconst(h);
-        if (cb) {
-            return unlockBuffer(*cb);
-        } else {
-            return -1;
-        }
-    }
-
-    static uint64_t getOffset(const buffer_handle_t h) {
-        const cb_handle_t* cb = cb_handle_t::from(h);
-        return cb ? cb->getMmapedOffset() : -1;
     }
 
 private:
