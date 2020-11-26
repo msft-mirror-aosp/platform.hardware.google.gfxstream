1cf3016630f9e86c07855591f53c96b7bdb1cbd7
Suraj Malhotra <surajmalhotra@google.com>
2020-11-25 17:31:11 -0800

    [logging] Use new logging macro and pass file and line
    This is part one in soft transition. It will be changed to use old
    logging macro name once transition is complete.
    
    Change-Id: I342d553d7588e966c8be970ae53cc267c000d0ba

--- fuchsia/port.cc
+++ fuchsia/port.cc
@@ -23,8 +23,8 @@ int property_get(const char* key, char* value, const char* default_value) {
   return 0;
 }
 
-int __android_log_print(int priority, const char* tag, const char* format,
-                        ...) {
+int __android_log_print(int priority, const char* tag, const char* file,
+                        int line, const char* format, ...) {
   const char* local_tag = tag;
   if (!local_tag) {
     local_tag = "<NO_TAG>";
@@ -34,34 +34,34 @@ int __android_log_print(int priority, const char* tag, const char* format,
   switch (priority) {
     case ANDROID_LOG_VERBOSE:
     case ANDROID_LOG_DEBUG:
-      FX_LOGVF(DEBUG, local_tag, format, ap);
+      FX_LOGVF2(DEBUG, local_tag, file, line, format, ap);
       break;
     case ANDROID_LOG_WARN:
-      FX_LOGVF(WARNING, local_tag, format, ap);
+      FX_LOGVF2(WARNING, local_tag, file, line, format, ap);
       break;
     case ANDROID_LOG_ERROR:
-      FX_LOGVF(ERROR, local_tag, format, ap);
+      FX_LOGVF2(ERROR, local_tag, file, line, format, ap);
       break;
     case ANDROID_LOG_FATAL:
-      FX_LOGVF(FATAL, local_tag, format, ap);
+      FX_LOGVF2(FATAL, local_tag, file, line, format, ap);
       break;
     case ANDROID_LOG_INFO:
     default:
-      FX_LOGVF(INFO, local_tag, format, ap);
+      FX_LOGVF2(INFO, local_tag, file, line, format, ap);
       break;
   }
   return 1;
 }
 
-void __android_log_assert(const char* condition, const char* tag,
-                          const char* format, ...) {
+void __android_log_assert(const char* condition, const char* file, int line,
+                          const char* tag, const char* format, ...) {
   const char* local_tag = tag;
   if (!local_tag) {
     local_tag = "<NO_TAG>";
   }
   va_list ap;
   va_start(ap, format);
-  FX_LOGVF(ERROR, local_tag, format, ap);
+  FX_LOGVF2(ERROR, local_tag, file, line, format, ap);
   va_end(ap);
 
   abort();
