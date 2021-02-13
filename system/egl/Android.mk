84f35fe63b6efefe4f58c4e679678964e93ba9ec
Bob Badour <bbadour@google.com>
2021-02-12 16:31:06 -0800

    [LSC] Add LOCAL_LICENSE_KINDS to device/generic/goldfish-opengl
    Added SPDX-license-identifier-Apache-2.0 to:
      shared/GoldfishAddressSpace/Android.bp
      shared/gralloc_cb/Android.bp
      shared/qemupipe/Android.bp
      system/codecs/c2/decoders/avcdec/Android.bp
      system/codecs/c2/decoders/base/Android.bp
      system/codecs/c2/decoders/vpxdec/Android.bp
      system/egl/Android.mk
      system/hals/Android.mk
      system/hwc2/Android.mk
      tests/gles_android_wrapper/Android.mk
    
    Added SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-BSD
        SPDX-license-identifier-GPL-2.0 SPDX-license-identifier-MIT
    to:
      Android.bp
    
    Added SPDX-license-identifier-Apache-2.0 SPDX-license-identifier-GPL-2.0
    to:
      android-emu/Android.bp
    
    Added SPDX-license-identifier-BSD
    to:
      system/codecs/c2/service/Android.bp
    
    Added SPDX-license-identifier-GPL-2.0
    to:
      system/codecs/c2/store/Android.bp
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    
    Exempt-From-Owner-Approval: janitorial work
    Change-Id: Ide2b5a35fe02f035b65aa0723799081e38292e7a

--- system/egl/Android.mk
+++ system/egl/Android.mk
@@ -55,6 +55,9 @@ ifneq (,$(filter aosp_arm aosp_x86 aosp_mips full full_x86 full_mips sdk sdk_x86
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := egl.cfg
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
+LOCAL_LICENSE_CONDITIONS := notice
+LOCAL_NOTICE_FILE := $(LOCAL_PATH)/../../LICENSE
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 
 LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/egl
