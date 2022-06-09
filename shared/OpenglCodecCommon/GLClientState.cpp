8b8c69b2ea860e9a524321f473ad93ed837576e5
Yahan Zhou <yahan@google.com>
2022-06-09 12:50:43 -0700

    Clean up RBO destroy code and fix regression
    Bug: 235127206
    Test: dEQP-GLES3.functional.fbo.api#invalid_rbo_attachments
    Change-Id: I0d5784a28df097b506840daec1bfbad7fe542dd0

--- shared/OpenglCodecCommon/GLClientState.cpp
+++ shared/OpenglCodecCommon/GLClientState.cpp
@@ -2105,7 +2105,6 @@ void GLClientState::addRenderbuffers(GLsizei n, GLuint* renderbuffers) {
 }
 
 void GLClientState::removeRenderbuffers(GLsizei n, const GLuint* renderbuffers) {
-    std::vector<std::shared_ptr<RboProps>> to_remove;
     bool unbindCurrent = false;
     {
         RenderbufferInfo::ScopedView view(mRboState.rboData);
@@ -2115,17 +2114,11 @@ void GLClientState::removeRenderbuffers(GLsizei n, const GLuint* renderbuffers) 
                 if (!rboPtr) {
                     continue;
                 }
-                to_remove.push_back(rboPtr);
+                unbindCurrent |=
+                        (mRboState.boundRenderbuffer == rboPtr);
                 setFboCompletenessDirtyForRbo(rboPtr);
+                view.remove(renderbuffers[i]);
             }
-        }
-
-        for (size_t i = 0; i < to_remove.size(); i++) {
-            if (mRboState.boundRenderbuffer == to_remove[i]) {
-                unbindCurrent = true;
-                break;
-            }
-            view.remove(to_remove[i]->id);
         }
     }
 
