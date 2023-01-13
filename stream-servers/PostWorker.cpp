ec04cc351f6e74cd8186049cb3637daa862428da
Yahan Zhou <yahan@google.com>
2023-01-13 15:30:07 -0800

    Fix foldable
    Bug: 265201744
    Change-Id: I5e61635053ace6896f2c104dc2c1e982cd5f2719

--- stream-servers/PostWorker.cpp
+++ stream-servers/PostWorker.cpp
@@ -112,6 +112,7 @@ std::shared_future<void> PostWorker::postImpl(ColorBuffer* cb) {
         .composeMode = HWC2_COMPOSITION_DEVICE,
         .blendMode = HWC2_BLEND_MODE_NONE,
         .transform = HWC_TRANSFORM_NONE,
+        .alpha = 1.0f,
     };
 
     const auto& multiDisplay = emugl::get_emugl_multi_display_operations();
