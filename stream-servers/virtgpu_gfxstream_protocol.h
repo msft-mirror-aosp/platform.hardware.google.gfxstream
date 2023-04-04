1b90709ddd23f1ab7c6fb08f701a652c29251c83
Bo Hu <bohu@google.com>
2023-04-04 01:42:55 +0000

    Revert "vulkan-cereal: guest memory prototype"
    Revert submission 2516960-gfxstream-guest-mem
    
    Reason for revert: broke windows build
    Reverted changes: /q/submissionid:2516960-gfxstream-guest-mem
    
    Change-Id: I396cc701bc74cf54383c47b08b09272f71723713

--- stream-servers/virtgpu_gfxstream_protocol.h
+++ stream-servers/virtgpu_gfxstream_protocol.h
@@ -31,11 +31,6 @@
 #define GFXSTREAM_CREATE_IMPORT_SYNC_VK         0xa001
 #define GFXSTREAM_CREATE_QSRI_EXPORT_VK         0xa002
 
-// clang-format off
-// A placeholder command to ensure virtio-gpu completes
-#define GFXSTREAM_PLACEHOLDER_COMMAND_VK        0xf002
-// clang-format on
-
 struct gfxstreamHeader {
     uint32_t opCode;
 };
@@ -70,12 +65,6 @@ struct gfxstreamCreateQSRIExportVK {
     uint32_t imageHandleHi;
 };
 
-struct gfxstreamPlaceholderCommandVk {
-    struct gfxstreamHeader hdr;
-    uint32_t pad;
-    uint32_t padding;
-};
-
 struct gfxstreamCapset {
     uint32_t protocolVersion;
 
@@ -84,8 +73,6 @@ struct gfxstreamCapset {
     uint32_t bufferSize;
 
     uint32_t pad;
-    uint32_t padding[16];
-    uint32_t deferredMapping;
 };
 
 #endif
