miyu-xu/hardware-google-gfxstream

Make VirtGpu* interfaces
... so that upcoming end2end tests can set up a mock impl that
interacts with the host within-process.

Bug: b/271464937
Test: m && cvd start
Change-Id: I9d1fbaf3c2e50831a03405ad4f37b477b224abd9

guest/OpenglSystemCommon/HostConnection.cpp
@@ -332,9 +332,8 @@ std::unique_ptr<HostConnection> HostConnection::connect(uint32_t capset_id) {
             break;
         }
         case HOST_CONNECTION_VIRTIO_GPU_ADDRESS_SPACE: {
-            VirtGpuDevice& instance =
-                VirtGpuDevice::getInstance((enum VirtGpuCapset)kCapsetGfxStream);
-            auto deviceHandle = instance.getDeviceHandle();
+            auto device = VirtGpuDevice::getInstance((enum VirtGpuCapset)kCapsetGfxStream);
+            auto deviceHandle = device->getDeviceHandle();
             auto stream = createVirtioGpuAddressSpaceStream(getGlobalHealthMonitor());
             if (!stream) {
                 ALOGE("Failed to create virtgpu AddressSpaceStream\n");

msft-mirror-aosp/platform.hardware.google.gfxstream

Bug: b/292257025

guest/OpenglSystemCommon/HostConnection.cpp
@@ -330,9 +330,8 @@ std::unique_ptr<HostConnection> HostConnection::connect(enum VirtGpuCapset capse
             break;
         }
         case HOST_CONNECTION_VIRTIO_GPU_ADDRESS_SPACE: {
-            VirtGpuDevice& instance =
-                VirtGpuDevice::getInstance((enum VirtGpuCapset)kCapsetGfxStreamVulkan);
-            auto deviceHandle = instance.getDeviceHandle();
+            auto device = VirtGpuDevice::getInstance((enum VirtGpuCapset)kCapsetGfxStreamVulkan);
+            auto deviceHandle = device->getDeviceHandle();
             auto stream = createVirtioGpuAddressSpaceStream(getGlobalHealthMonitor());
             if (!stream) {
                 ALOGE("Failed to create virtgpu AddressSpaceStream\n");