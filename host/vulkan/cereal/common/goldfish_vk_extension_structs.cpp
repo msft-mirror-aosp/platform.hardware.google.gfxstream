0de3a225b82121b8ce4a9335befa2cb3e63c5ec4
Lars Harrison <slumpwuffle@google.com>
2023-12-20 22:59:00 +0000

    Provide error message on unhandled VK structs
    This change provides more explicit error messages and a proper abort in
    situations where an unhandled VK structure is attempting to be used,
    based purely on calls to the size functions. This should enable better
    capture and debugging of these failures in the future (rather than
    hitting a segfault and having to diagnose).
    
    Bug: 317276564
    Test: built with changes and ran locally
    Change-Id: Ib00af771886823efa31e83fa0e1a557216557569

--- host/vulkan/cereal/common/goldfish_vk_extension_structs.cpp
+++ host/vulkan/cereal/common/goldfish_vk_extension_structs.cpp
@@ -778,6 +778,8 @@ size_t goldfish_vk_extension_struct_size(VkStructureType rootType, const void* s
         }
 #endif
         default: {
+            fprintf(stderr, "Unhandled Vulkan structure type %d, aborting.\n", structType);
+            GFXSTREAM_ABORT(::emugl::FatalError(::emugl::ABORT_REASON_OTHER));
             return (size_t)0;
         }
     }
@@ -1354,6 +1356,8 @@ size_t goldfish_vk_extension_struct_size_with_stream_features(uint32_t streamFea
         }
 #endif
         default: {
+            fprintf(stderr, "Unhandled Vulkan structure type %d, aborting.\n", structType);
+            GFXSTREAM_ABORT(::emugl::FatalError(::emugl::ABORT_REASON_OTHER));
             return (size_t)0;
         }
     }
