6ed12e9d6687b1bffa62d8b85f8a45219f61c5ac
Gurchetan Singh <gurchetansingh@google.com>
2024-01-19 02:19:50 +0000

    Revert "gfxstream: introduce GFXSTREAM_ENABLE_HOST_GLES compiler flag"
    This reverts commit 4675a9d4ef76b83dea5367079c8415102d3781da.
    
    Reason for revert: I suppose it breaks MacOS
    
    Change-Id: I91bef5c2fa0e8726b6e77e6678540b808ad26316

--- host/NativeSubWindow.h
+++ host/NativeSubWindow.h
@@ -18,11 +18,7 @@
 
 #include "render-utils/render_api_platform_types.h"
 
-#if GFXSTREAM_ENABLE_HOST_GLES
 #include <EGL/egl.h>
-#else
-#include "GlesCompat.h"
-#endif
 
 #ifdef __cplusplus
 extern "C" {
