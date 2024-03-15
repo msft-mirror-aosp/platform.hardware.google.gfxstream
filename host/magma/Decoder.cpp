b45c014871f0e0d3669484532c6dd3d7e49b2c78
Erwin Jansen <jansene@google.com>
2024-03-15 14:36:45 -0700

    Enable GFXStream bazel build
    This builds the "standalone" GFXStream library that can be consumed by
    Qemu 8.2
    
    The bazel build allows us to transition rutabaga to a bazel based
    build, which will unify the Qemu 8.2 build as used by the emulator.
    
    It also opens up the path to migrate this to g3 if we decide to do this.
    
    This change forces a few header changes, as bazel is more strict about
    the relationship between headers and packages, and doesn't easily give
    you fine grained control over the include paths.
    
    Change-Id: I3566d69f10c534f1176fa92eaa7f5f12ec0bcd4f

--- host/magma/Decoder.cpp
+++ host/magma/Decoder.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "host/magma/Decoder.h"
+#include "Decoder.h"
 
 #include "RenderThreadInfoMagma.h"
 
