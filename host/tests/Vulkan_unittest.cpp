miyu-xu/hardware-google-gfxstream

Introduce Gfxstream Features to decouple Gfxstream from AEMU
... which will allow building Gfxstream from a fixed version of
AEMU for the purposes of packaging.

Bug: b/326648397
Test: cvd start --gpu_mode=gfxstream
Test: cvd start --gpu_mode=gfxstream_guest_angle
Change-Id: I8ac1622428f0194997597868bef49118bcc3ea90

host/tests/Vulkan_unittest.cpp
@@ -17,7 +17,6 @@
 #include "FrameBuffer.h"
 #include "VkCommonOperations.h"
 #include "VulkanDispatch.h"
-#include "host-common/feature_control.h"
 
 #include "aemu/base/ArraySize.h"
 #include "aemu/base/GLObjectCounter.h"
@@ -436,10 +435,11 @@ protected:
         // the rendering tests on Windows for now.
         SKIP_TEST_IF_WIN32();
 
-        feature_set_enabled_override(kFeature_GLESDynamicVersion, true);
-        feature_set_enabled_override(kFeature_PlayStoreImage, false);
-        feature_set_enabled_override(kFeature_Vulkan, true);
-        feature_set_enabled_override(kFeature_VulkanIgnoredHandles, true);
+        gfxstream::host::FeatureSet features;
+        features.GlesDynamicVersion.enabled = true;
+        features.PlayStoreImage.enabled = true;
+        features.Vulkan.enabled = true;
+        features.VulkanIgnoresHandles.enabled = true;
 
         VulkanTest::SetUp();
 
@@ -450,7 +450,7 @@ protected:
         ASSERT_NE(nullptr, gl::LazyLoadedGLESv2Dispatch::get());
 
         bool useHostGpu = false;
-        EXPECT_TRUE(FrameBuffer::initialize(mWidth, mHeight, false,
+        EXPECT_TRUE(FrameBuffer::initialize(mWidth, mHeight, features, false,
                                             !useHostGpu /* egl2egl */));
         mFb = FrameBuffer::getFB();
         ASSERT_NE(nullptr, mFb);

msft-mirror-aosp/platform.hardware.google.gfxstream

host/tests/Vulkan_unittest.cpp
@@ -17,7 +17,6 @@
 #include "FrameBuffer.h"
 #include "VkCommonOperations.h"
 #include "VulkanDispatch.h"
-#include "host-common/feature_control.h"
 
 #include "aemu/base/ArraySize.h"
 #include "aemu/base/GLObjectCounter.h"
@@ -436,10 +435,11 @@ protected:
         // the rendering tests on Windows for now.
         SKIP_TEST_IF_WIN32();
 
-        feature_set_enabled_override(kFeature_GLESDynamicVersion, true);
-        feature_set_enabled_override(kFeature_PlayStoreImage, false);
-        feature_set_enabled_override(kFeature_Vulkan, true);
-        feature_set_enabled_override(kFeature_VulkanIgnoredHandles, true);
+        gfxstream::host::FeatureSet features;
+        features.GlesDynamicVersion.enabled = true;
+        features.PlayStoreImage.enabled = true;
+        features.Vulkan.enabled = true;
+        features.VulkanIgnoredHandles.enabled = true;
 
         VulkanTest::SetUp();
 
@@ -450,7 +450,7 @@ protected:
         ASSERT_NE(nullptr, gl::LazyLoadedGLESv2Dispatch::get());
 
         bool useHostGpu = false;
-        EXPECT_TRUE(FrameBuffer::initialize(mWidth, mHeight, false,
+        EXPECT_TRUE(FrameBuffer::initialize(mWidth, mHeight, features, false,
                                             !useHostGpu /* egl2egl */));
         mFb = FrameBuffer::getFB();
         ASSERT_NE(nullptr, mFb);