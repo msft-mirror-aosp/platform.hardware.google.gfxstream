acc72c6522499a87bcdb62c2dfe9429417bac584
Jason Macnak <natsu@google.com>
2024-03-19 14:37:38 -0700

    Ensure glProgramBinary initializes uniform and attrib info
    Adds Scoped* helper classes to hopefully make writing tests easier.
    
    Bug: b/330392312
    Test: GfxstreamEnd2EndTests
    Change-Id: Ibb2c957682dd7173bd6f839bc1c379d47c4e0318

--- guest/platform/rutabaga/RutabagaLayer.h
+++ guest/platform/rutabaga/RutabagaLayer.h
@@ -68,8 +68,13 @@ class EmulatedVirtioGpu {
 
    int TransferFromHost(uint32_t contextId, uint32_t resourceId, uint32_t transferOffset,
                         uint32_t transferSize);
+   int TransferFromHost(uint32_t contextId, uint32_t resourceId, uint32_t x, uint32_t y, uint32_t w,
+                        uint32_t h);
+
    int TransferToHost(uint32_t contextId, uint32_t resourceId, uint32_t transferOffset,
                       uint32_t transferSize);
+   int TransferToHost(uint32_t contextId, uint32_t resourceId, uint32_t x, uint32_t y, uint32_t w,
+                      uint32_t h);
 
    void SignalEmulatedFence(int fenceId);
 
