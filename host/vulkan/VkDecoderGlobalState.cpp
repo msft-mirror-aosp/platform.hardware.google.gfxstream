88df7cff15303649c75e2d4ed5d3dd8a48b48b4b
Yahan Zhou <yahan@google.com>
2024-04-15 15:43:47 -0700

    Fix ColorBufferVk layout on snapshot load again
    We have a situation that the color buffer vulkan images are transformed
    into the wrong layout after snapshot load. Fix it by memorizing the
    layout in snapshot.
    
    We don't have a test for it yet as we don't really understand how to
    trigger this behavior at unittest level.
    
    Also recover layout for multi-sample images after snapshot loads. But we
    still don't snapshot its content yet.
    
    Bug: 332775761
    Test: CanSubmitQueue
    Change-Id: I8007f504d055041b11c3af3425cfbdbfac0553dd

--- host/vulkan/VkDecoderGlobalState.cpp
+++ host/vulkan/VkDecoderGlobalState.cpp
@@ -6249,6 +6249,7 @@ class VkDecoderGlobalState::Impl {
     GOLDFISH_VK_LIST_NON_DISPATCHABLE_HANDLE_TYPES(DEFINE_BOXED_NON_DISPATCHABLE_HANDLE_API_IMPL)
 
     VkDecoderSnapshot* snapshot() { return &mSnapshot; }
+    SnapshotState getSnapshotState() { return mSnapshotState; }
 
    private:
     bool isEmulatedInstanceExtension(const char* name) const {
@@ -7148,11 +7149,6 @@ class VkDecoderGlobalState::Impl {
     std::unordered_map<LinearImageCreateInfo, LinearImageProperties, LinearImageCreateInfo::Hash>
         mLinearImageProperties;
 
-    enum SnapshotState {
-        Normal,
-        Saving,
-        Loading,
-    };
     SnapshotState mSnapshotState = SnapshotState::Normal;
 };
 
@@ -7177,6 +7173,10 @@ void VkDecoderGlobalState::reset() {
 
 // Snapshots
 bool VkDecoderGlobalState::snapshotsEnabled() const { return mImpl->snapshotsEnabled(); }
+
+VkDecoderGlobalState::SnapshotState VkDecoderGlobalState::getSnapshotState() const {
+    return mImpl->getSnapshotState();
+}
 
 const gfxstream::host::FeatureSet& VkDecoderGlobalState::getFeatures() const { return mImpl->getFeatures(); }
 
