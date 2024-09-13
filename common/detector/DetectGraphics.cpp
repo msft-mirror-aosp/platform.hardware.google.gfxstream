1af6f6bbaa4bbca44e403963aac5381cfeb7bd93
Jason Macnak <natsu@google.com>
2024-09-13 05:42:35 +0000

    Add option to write to file to graphics detector
    ... as otherwise running with VK_LAYER_LUNARG_api_dump or
    VK_LAYER_KHRONOS_validation will generate additional output and
    break proto parsing.
    
    Bug: b/366231094
    Test: cvd create
    Change-Id: Id7c09f6e275ad3c3a8963e9c401d5b257399b3ef

--- common/detector/DetectGraphics.cpp
+++ common/detector/DetectGraphics.cpp
@@ -14,19 +14,45 @@
  * limitations under the License.
  */
 
-#include <string>
-
 #include <google/protobuf/text_format.h>
+
+#include <fstream>
+#include <string>
 
 #include "GraphicsDetector.h"
 #include "GraphicsDetector.pb.h"
 
-int main(int, char*[]) {
+namespace {
+
+bool WriteToFile(const std::string filename, const std::string contents) {
+    std::ofstream ofs(filename);
+    if (!ofs.is_open()) {
+        return false;
+    }
+    ofs << contents;
+    ofs.close();
+    return !ofs.fail();
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
     const auto availability = ::gfxstream::DetectGraphicsAvailability();
 
-    std::string s;
-    if (google::protobuf::TextFormat::PrintToString(availability, &s)) {
-        std::cout << s << std::endl;
+    std::string availabilityString;
+    if (!google::protobuf::TextFormat::PrintToString(availability, &availabilityString)) {
+        std::cerr << "Failed to convert availability to string." << std::endl;
+        return -1;
+    }
+
+    if (argc > 1) {
+        const std::string filename = argv[1];
+        if (!WriteToFile(filename, availabilityString)) {
+            std::cerr << "Failed to write to '" << filename << "'.";
+            return -1;
+        }
+    } else {
+        std::cout << availabilityString << std::endl;
     }
 
     return 0;
